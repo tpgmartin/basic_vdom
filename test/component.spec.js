472893f11bcf92297c1b2aa86ebcb946255c0f1d
Tom Martin <tom@tpgmartin.com>
2017-01-04 09:01:22 +0000

    Handle nested components

--- test/component.spec.js
+++ test/component.spec.js
@@ -11,4 +11,27 @@ describe('Component', () => {
     expect(component.props).to.deep.equal({})
     expect(component.children).to.deep.equal([])
   })
+
+  it('should handle children components', () => {
+    const component = c('div', {name: 'root'}, [
+      c('div', {name: 'child'})
+    ])
+
+    expect(component).to.be.an('object')
+    expect(component.tagName).to.equal('div')
+    expect(component.props).to.deep.equal({name: 'root'})
+    expect(component.children[0].tagName).to.equal('div')
+    expect(component.children[0].props).to.deep.equal({name: 'child'})
+  })
+
+  it('should create stateless components', () => {
+    const component = c('div', {}, [
+      c('div', {name: 'child'})
+    ])
+
+    expect(component).to.be.an('object')
+    expect(component.tagName).to.equal('div')
+    expect(component.children[0].tagName).to.equal('div')
+    expect(component.children[0].props).to.deep.equal({name: 'child'})
+  })
 })
