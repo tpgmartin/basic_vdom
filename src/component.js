d4920898a95ef64c479c133820b8a74d269bab76
Tom Martin <tom@tpgmartin.com>
2017-01-04 19:54:05 +0000

    Setup render function

--- src/component.js
+++ src/component.js
@@ -1,3 +1,15 @@
-export default function Component(tagName, props={}, children=[]) {
-  return { tagName, props, children }
+export default function Component(tagName, props = {}, children = []) {
+
+  if (!(this instanceof Component)) {
+    return new Component(tagName, props, children)
+  }
+
+  this.tagName = tagName
+  this.props = props
+  this.children = children
+
+}
+
+Component.prototype.render = function () {
+  return document.createElement(this.tagName)
 }
--- test/component.spec.js
+++ test/component.spec.js
@@ -1,37 +1,59 @@
 import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
 import c from '../src/component'
+chai.use(sinonChai)
 
 describe('Component', () => {
 
-  it('should return new component', () => {
-    const component = c('div')
+  describe('creation', () => {
 
-    expect(component).to.be.an('object')
-    expect(component.tagName).to.equal('div')
-    expect(component.props).to.deep.equal({})
-    expect(component.children).to.deep.equal([])
+    it('should return new component', () => {
+      const component = c('div')
+
+      expect(component).to.be.an('object')
+      expect(component.tagName).to.equal('div')
+      expect(component.props).to.deep.equal({})
+      expect(component.children).to.deep.equal([])
+    })
+
+    it('should handle children components', () => {
+      const component = c('div', { name: 'root' }, [
+        c('div', { name: 'child' })
+      ])
+
+      expect(component).to.be.an('object')
+      expect(component.tagName).to.equal('div')
+      expect(component.props).to.deep.equal({ name: 'root' })
+      expect(component.children[0].tagName).to.equal('div')
+      expect(component.children[0].props).to.deep.equal({ name: 'child' })
+    })
+
+    it('should create stateless components', () => {
+      const component = c('div', {}, [
+        c('div', { name: 'child' })
+      ])
+
+      expect(component).to.be.an('object')
+      expect(component.tagName).to.equal('div')
+      expect(component.children[0].tagName).to.equal('div')
+      expect(component.children[0].props).to.deep.equal({ name: 'child' })
+    })
+
   })
 
-  it('should handle children components', () => {
-    const component = c('div', {name: 'root'}, [
-      c('div', {name: 'child'})
-    ])
+  describe('render', () => {
 
-    expect(component).to.be.an('object')
-    expect(component.tagName).to.equal('div')
-    expect(component.props).to.deep.equal({name: 'root'})
-    expect(component.children[0].tagName).to.equal('div')
-    expect(component.children[0].props).to.deep.equal({name: 'child'})
+    it('should return new DOM element', () => {
+      sinon.stub(document.prototype, 'createElement')
+      const component = c('div')
+      const element = component.render()
+      sinon.spy(c.prototype, 'render');
+      expect(element).to.be.an('object')
+      expect(c.prototype.render).to.have.been.calledOnce
+      expect(element).to.be.an.instanceof('Element');
+    })
+
   })
 
-  it('should create stateless components', () => {
-    const component = c('div', {}, [
-      c('div', {name: 'child'})
-    ])
-
-    expect(component).to.be.an('object')
-    expect(component.tagName).to.equal('div')
-    expect(component.children[0].tagName).to.equal('div')
-    expect(component.children[0].props).to.deep.equal({name: 'child'})
-  })
 })
